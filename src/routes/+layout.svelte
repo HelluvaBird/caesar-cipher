1e671aa03fa3c627883362546b94325598212ce2
HelluvaBird <royceman24@hotmail.com>
2024-02-08 21:03:15 -0500

    redesign site

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<title>Caesar Cipher</title>
+</svelte:head>
+
 <main class="min-h-screen grid place-items-center">
 	<slot />
 </main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
-	let decrypt = false;
-	let bruteForce = false;
+	let action: (() => void) | null = null;
 	let shift = 0;
 	let text = '';
 	const plaintext = 'abcdefghijklmnopqrstuvwxyz';
 	$: cipher = plaintext.slice(shift) + plaintext.slice(0, shift);
 
 	const encryptText = () => {
-		if (text.length <= 0) return;
+		if (!action || shift === 0 || text.length <= 0) return;
 		let encryptedText = '';
 		for (const char of text) {
 			if (char.match(/[\s.,;:!?"'\-]/gi)) {
@@ -26,7 +25,7 @@
 	};
 
 	const decryptText = () => {
-		if (text.length <= 0) return;
+		if (!action || shift === 0 || text.length <= 0) return;
 		let decryptedText = '';
 		for (const char of text) {
 			if (char.match(/[\s.,;:!?"'\-]/gi)) {
@@ -45,7 +44,7 @@
 	};
 
 	const bruteforce = () => {
-		if (text.length <= 0) return;
+		if (!action || shift === 0 || text.length <= 0) return;
 		const originalText = text;
 		let hugeText = 'original text: ' + originalText + '\n';
 
@@ -73,108 +72,95 @@
 	};
 </script>
 
-<section class="min-h-96 max-w-6xl w-full bg-gray-700 p-4 rounded-md">
-	<h1 class="text-center text-4xl">The Caesar Cipher</h1>
-	<hr />
-	<div class="space-y-5 mt-5">
-		<div class="flex items-center gap-4">
-			<!-- Enabled: "bg-indigo-600", Not Enabled: "bg-gray-200" -->
-			<div class="flex items-center">
-				<span class="mr-3" id="encryptdecrypt">
-					<span class="text-sm font-medium">Encrypt</span>
-				</span>
-				<button
-					type="button"
-					on:click={() => (decrypt = !decrypt)}
-					class="relative bg-indigo-600 inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-					role="switch"
-					aria-checked={decrypt}
-					aria-labelledby="encryptdecrypt"
-				>
-					<!-- Enabled: "translate-x-5", Not Enabled: "translate-x-0" -->
-					<span
-						aria-hidden="true"
-						class:translate-x-0={!decrypt}
-						class:translate-x-5={decrypt}
-						class="pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200"
-					></span>
-				</button>
-				<span class="ml-3" id="encryptdecrypt">
-					<span class="text-sm font-medium">Decrypt</span>
-				</span>
-			</div>
-			<div
-				class="flex items-center transition-opacity ease-in-out duration-200"
-				class:opacity-0={!decrypt}
-				class:opacity-100={decrypt}
-			>
-				<button
-					type="button"
-					on:click={() => (bruteForce = !bruteForce)}
-					class:bg-gray-200={!bruteForce}
-					class:bg-indigo-600={bruteForce}
-					class="relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-					role="switch"
-					aria-checked={bruteForce}
-					aria-labelledby="bruteforceattack"
-				>
-					<!-- Enabled: "translate-x-5", Not Enabled: "translate-x-0" -->
-					<span
-						aria-hidden="true"
-						class:translate-x-0={!bruteForce}
-						class:translate-x-5={bruteForce}
-						class="pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200"
-					></span>
-				</button>
-				<span class="ml-3" id="bruteforceattack">
-					<span class="text-sm font-medium">Bruteforce</span>
-				</span>
+<section class="max-w-6xl w-full bg-gray-700 p-4 rounded-md">
+	<div class="md:grid md:grid-cols-3 md:gap-6">
+		<div class="md:col-span-1">
+			<div class="">
+				<h1 class="text-4xl">The Caesar Cipher</h1>
+				<p class="mt-1 text-sm text-gray-400">some info about the caesar cipher here.</p>
 			</div>
 		</div>
-		<div>
-			<label for="cipher" class="block text-sm font-medium">Shift Cipher</label>
-			<div class="mt-1">
-				<input
-					type="number"
-					name="cipher"
-					id="cipher"
-					bind:value={shift}
-					min="0"
-					max="25"
-					class="text-gray-900 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block border-gray-300 rounded-md ps-1"
-				/>
+		<div class="mt-5 md:mt-0 md:col-span-2">
+			<fieldset>
+				<div>
+					<legend class="text-xl font-medium">Actions</legend>
+					<p class="text-sm text-gray-400">These are the actions you can perform on the text.</p>
+				</div>
+				<div class="mt-4 space-y-4 md:flex md:space-x-4 md:space-y-0">
+					<div class="flex items-center">
+						<input
+							id="encrypt"
+							name="cipher-action"
+							type="radio"
+							class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
+							on:change={(event) => (action = encryptText)}
+						/>
+						<label for="encrypt" class="ml-3 block text-sm font-medium">Encrypt</label>
+					</div>
+					<div class="flex items-center">
+						<input
+							id="decrypt"
+							name="cipher-action"
+							type="radio"
+							class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
+							on:change={(event) => (action = decryptText)}
+						/>
+						<label for="decrypt" class="ml-3 block text-sm font-medium">Decrypt</label>
+					</div>
+					<div class="flex items-center">
+						<input
+							id="bruteforce"
+							name="cipher-action"
+							type="radio"
+							class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300"
+							on:change={(event) => (action = bruteforce)}
+						/>
+						<label for="bruteforce" class="ml-3 block text-sm font-medium"> Bruteforce </label>
+					</div>
+				</div>
+			</fieldset>
+			<div class="mt-4">
+				<div>
+					<label for="cipher" class="text-xl font-medium">Shift Cipher</label>
+					<p class="text-sm text-gray-400">The number of times to shift the cipher.</p>
+				</div>
+
+				<div class="mt-4">
+					<input
+						type="number"
+						name="cipher"
+						id="cipher"
+						bind:value={shift}
+						min="0"
+						max="25"
+						class="text-gray-900 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block border-gray-300 rounded-md ps-1 w-1/2 md:w-auto"
+					/>
+				</div>
 			</div>
-		</div>
-		<div>
-			<label for="text" class="block text-sm font-medium"
-				>Text to {bruteForce && decrypt ? 'bruteforce' : decrypt ? 'decrypt' : 'encrypt'}</label
-			>
-			<div class="mt-1">
-				<textarea
-					rows="4"
-					name="text"
-					id="text"
-					bind:value={text}
-					class="text-gray-900 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full border-gray-300 rounded-md px-1"
-				></textarea>
+			<div class="mt-4">
+				<div>
+					<label for="text" class="text-xl font-medium">Text</label>
+					<p class="text-sm text-gray-400">the text you wish to perform the action on.</p>
+				</div>
+
+				<div class="mt-4">
+					<textarea
+						rows="4"
+						name="text"
+						id="text"
+						bind:value={text}
+						class="text-gray-900 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full border-gray-300 rounded-md px-1"
+					></textarea>
+				</div>
 			</div>
-		</div>
-		<div class="flex justify-end">
-			{#if bruteForce && decrypt}
+			<div class="mt-4 flex justify-end">
 				<button
-					on:click={bruteforce}
-					type="button"
-					class="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
-					>Bruteforce text</button
-				>
-			{:else}
-				<button
-					on:click={decrypt ? decryptText : encryptText}
+					on:click={action}
 					type="button"
 					class="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-					>{bruteForce && decrypt ? 'Bruteforce' : decrypt ? 'Decrypt' : 'Encrypt'} text</button
+					>text</button
 				>
-			{/if}
+			</div>
 		</div>
 	</div>
 </section>
